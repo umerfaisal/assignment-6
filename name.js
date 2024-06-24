feea21c0fbc33744081abe7f5daeb0a00fc8a234
umerfaisal <109797391+umerfaisal@users.noreply.github.com>
2024-06-25 00:09:28 +0500

    Add files via upload

---
+++ name.js
@@ -0,0 +1,4 @@
+var whiteSpaceName = " \n\tUmer Faisal\t\n";
+console.log(whiteSpaceName);
+var withoutWhiteSpace = whiteSpaceName.trim();
+console.log(withoutWhiteSpace);
---
+++ name.ts
@@ -0,0 +1,5 @@
+let whiteSpaceName = " \n\tUmer Faisal\t\n"
+console.log(whiteSpaceName);
+
+let withoutWhiteSpace = whiteSpaceName.trim();
+console.log(withoutWhiteSpace);
